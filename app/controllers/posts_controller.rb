067e499c5e93dbdfbfdb5fc9b501d95b1d57f84b
Emily Wivell <emilywivell@Emilys-MacBook-Pro.local>
2018-02-09 16:20:32 -0500

    added small amount of form validation and showing errors to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,28 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.sort
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
-  end
-
-  def show
-    @post = Post.find(id=params[:id])
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
