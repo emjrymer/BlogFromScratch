a648b33f7643f50c6d4dafbf0eafc1a808691427
Emily Wivell <emilywivell@Emilys-MacBook-Pro.local>
2018-02-12 11:21:28 -0500

    added comment model, controller, and views to add and delete comments for each blog post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :author
   validates :title, presence: true, length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209181949) do
+ActiveRecord::Schema.define(version: 20180212150035) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "author"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_authors_on_post_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
